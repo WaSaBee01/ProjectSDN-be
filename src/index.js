7262057639272e4ef4c314ae50b692a7b3ae41d2
WaSaBee <ace81308@gmail.com>
2025-09-10 10:00:00 +0700

    test commit

--- src/index.js
+++ src/index.js
@@ -31,3 +31,4 @@ mongoose.connect(`${process.env.MONGO_DB}`)
 app.listen(port, () => {
     console.log(`Server is running on port: `, + port)
 })
+
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,8 +8,9 @@ const routes = (app) => {
   app.use("/api/user", UserRouter);
   app.use("/api/product", ProductRouter);
   app.use("/api/order", OrderRouter);
+app.use("/api/chat", ChatRouter);
   app.use("/api/payment", PaymentRouter);
-  app.use("/api/chat", ChatRouter);
+  
 };
 
 module.exports = routes;
