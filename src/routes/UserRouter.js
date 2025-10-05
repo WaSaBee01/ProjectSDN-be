9fc53e4ca8aa43e1b963f20747e7761b4769c5d2
WaSaBee <ace81308@gmail.com>
2025-10-05 10:00:00 +0700

    fix loi onboard

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -9,11 +9,11 @@ const {
 router.post("/sign-up", userController.createUser);
 router.post("/sign-in", userController.loginUser);
 router.post("/log-out", userController.logoutUser);
-router.put("/update-user/:id", authUserMiddleware, userController.updateUser);
-router.delete("/delete-user/:id", authMiddleware, userController.deleteUser);
-router.get("/getAll", authMiddleware, userController.getAllUser);
 router.get("/get-details/:id", userController.getDetailsUser);
 router.post("/refresh-token", userController.refreshToken);
 router.post("/delete-many", authMiddleware, userController.deleteMany);
+router.put("/update-user/:id", authUserMiddleware, userController.updateUser);
+router.delete("/delete-user/:id", authMiddleware, userController.deleteUser);
+router.get("/getAll", authMiddleware, userController.getAllUser);
 
 module.exports = router;
